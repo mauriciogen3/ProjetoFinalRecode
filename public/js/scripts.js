0336b1063fb8d86b4c31be3b76b073de05dd4b8d
mau086587 <mauriciogen3@hotmail.com>
2021-08-23 16:27:43 -0300

    criando tela de cadastro

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -34,7 +34,19 @@ button.addEventListener('click',(event)=> {
     password.classList.add("errorInput")
   } else{
     password.classList.remove.remove("errorInput")
-  }
+  } 
 
-  
+})
+
+let btn = document.querySelector('.fa-eye')
+
+btn.addEventListener('click',()=>{
+    let inputSenha = document.querySelector('#password')
+
+    if(inputSenha.getAttribute('type') =='password'){
+        inputSenha.setAttribute('type', 'text')
+    } else{
+        inputSenha.setAttribute('type', 'password')
+
+    }
 })
