f8be15c6e0b28e31d4a5cc4d32d6bb365ff78630
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-08-23 00:25:23 -0300

    Tela Login e Autenticação

---
+++ public/js/scripts.js
@@ -0,0 +1,36 @@
+const button = document.getElementById('button')
+
+button.addEventListener('click',(event)=> {
+  event.preventDefault()
+
+  //alert('botao acionado')
+
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+
+  if (email.value==''){
+    email.classList.add("errorInput")
+  } else{
+    email.classList.remove("errorInput")
+
+  if (password.value==''){
+    password.classList.add("errorInput")
+  } else{
+    password.classList.remove("errorInput")
+
+  if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1 || (email.value.indexOf(".") - email.value.indexOf("@") == 1)) {
+    email.classList.add("errorInput")
+  } else{
+    email.classList.remove("errorInput")
+  }
+
+  if (!isNaN(email.value) == true && email.value.length == 11){
+    email.classList.remove("errorInput")
+  } 
+
+  if(password.value.length <=5){
+    password.classList.add("errorInput")
+  } else{
+    password.classList.remove.remove("errorInput")
+  }
+})
